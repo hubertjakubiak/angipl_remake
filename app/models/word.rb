421fd3351868ffbb31ebbc34fc4f7249680f5118
Hubert Jakubiak <hubert.jakubiak@netguru.co>
2018-11-03 18:49:42 +0100

    Add new relationships for word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,12 @@
 class Word < ApplicationRecord
   belongs_to :language
   belongs_to :user
+
+  has_many :translations_association, class_name: 'Translation'
+  has_many :translations, through: :translations_association, source: :translated_word
+  has_many :inverse_translations_association, class_name: 'Translation', foreign_key: 'translated_word_id'
+  has_many :inverse_translations, through: :inverse_translations_association, source: :word
+
   validates :content, :language, presence: true
 
   paginates_per 10
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -8,6 +8,25 @@ RSpec.describe Word, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:language) }
     it { is_expected.to belong_to(:user) }
+    it do
+      is_expected.to have_many(:translations_association)
+        .class_name('Translation')
+    end
+    it do
+      is_expected.to have_many(:translations)
+        .through(:translations_association)
+        .source(:translated_word)
+    end
+    it do
+      is_expected.to have_many(:inverse_translations_association)
+        .class_name('Translation')
+        .with_foreign_key('translated_word_id')
+    end
+    it do
+      is_expected.to have_many(:inverse_translations)
+        .through(:inverse_translations_association)
+        .source(:word)
+    end
   end
 
   describe 'validations' do
