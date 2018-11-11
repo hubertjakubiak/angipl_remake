2e757cd1eac4015aca68a2d5b1581beb959174a8
Hubert Jakubiak <hubert.jakubiak@netguru.co>
2018-11-11 12:08:50 +0100

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@ language_1 = Language.create(name: 'English')
 language_2 = Language.create(name: 'Polish')
 
 5.times do
-  Word.create(content: Faker::Lorem.word, language: language_1, user: user_1)
-  Word.create(content: Faker::Lorem.word, language: language_2, user: user_1)
+  word = Word.create(content: Faker::Lorem.word, language: language_1, user: user_1)
+  translation_1 = Word.create(content: Faker::Lorem.word, language: language_2, user: user_1)
+  translation_2 = Word.create(content: Faker::Lorem.word, language: language_2, user: user_1)
+  word.translations << [translation_1, translation_2]
 end
 
 5.times do
