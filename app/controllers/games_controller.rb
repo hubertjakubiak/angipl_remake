3088c227c97f267b2affd58346a80facd7329800
Hubert Jakubiak <hubert.jakubiak@netguru.co>
2018-11-19 17:04:56 +0100

    Add controller for games

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  before_action :authenticate_user!, only: %i[create show]
+
+  def create
+    game = current_user.games.create
+    redirect_to(game)
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+end
