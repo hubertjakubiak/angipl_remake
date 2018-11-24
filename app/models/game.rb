583946b6134cb40dcad3145ba42950280ccfd541
Hubert Jakubiak <hubert.jakubiak@netguru.co>
2018-11-24 13:31:19 +0100

    Add correctness method to Game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,16 @@
 class Game < ApplicationRecord
   belongs_to :user
+
+  def correctness
+    return '-' if total_answers_count.zero?
+    value = (good_answers_count.to_f / total_answers_count) * 100
+    value = value.round(2)
+    value.to_s + '%'
+  end
+
+  private
+
+  def total_answers_count
+    good_answers_count + bad_answers_count
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,4 +9,58 @@ RSpec.describe Game, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe '#correctness' do
+    subject { game.correctness }
+
+    let(:game) do
+      build_stubbed(
+        :game,
+        good_answers_count: good_answers_count,
+        bad_answers_count: bad_answers_count
+      )
+    end
+
+    context 'when good_answers_count is 0' do
+      let(:good_answers_count) { 0 }
+
+      context 'when bad_answers_count is 0' do
+        let(:bad_answers_count) { 0 }
+
+        it { is_expected.to eq('-') }
+      end
+
+      context 'when bad_answers_count is 1' do
+        let(:bad_answers_count) { 1 }
+
+        it { is_expected.to eq('0.0%') }
+      end
+    end
+
+    context 'when good_answers_count is 1' do
+      let(:good_answers_count) { 1 }
+
+      context 'when bad_answers_count is 0' do
+        let(:bad_answers_count) { 0 }
+
+        it { is_expected.to eq('100.0%') }
+      end
+
+      context 'when bad_answers_count is 1' do
+        let(:bad_answers_count) { 1 }
+
+        it { is_expected.to eq('50.0%') }
+      end
+    end
+
+    context 'when good_answers_count is 2' do
+      let(:good_answers_count) { 2 }
+
+      context 'when bad_answers_count is 4' do
+        let(:bad_answers_count) { 4 }
+
+        it { is_expected.to eq('33.33%') }
+      end
+    end
+  end
 end
